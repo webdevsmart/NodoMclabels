b6805b05a801d5845e2d4f90f6c8ddf221603f8f
gentleman1122 <reader2027@gmail.com>
2021-07-10 02:09:45 -0700

    fix loop

--- index.js
+++ index.js
@@ -7,31 +7,31 @@ let products = [];
 let flag = true;
 let i = 1;
 async function getProducts(page) {
-  //  while (flag) {
-  await axios
-    .get("http://nodo.mclabels.com/export_prods/0", {
-      params: {
-        token:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImluZm9Ac2hvY2xlZmNvcnBvcmF0aW9uLmNvbSJ9.Uyn0FgRwtYZH-cOEcs1e4emP55xkDag2ZetAXa_ok9U",
-        page: i,
-      },
-    })
-    .then(async (res) => {
-      if (res.data.length != 0) {
-        await res.data.map((item, key) => {
-          products.push(item);
-        });
-        console.log("done : ", i);
-        i++;
-      } else {
-        flag = false;
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-    .then(() => {});
-  // }
+  while (flag) {
+    await axios
+      .get("http://nodo.mclabels.com/export_prods/0", {
+        params: {
+          token:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImluZm9Ac2hvY2xlZmNvcnBvcmF0aW9uLmNvbSJ9.Uyn0FgRwtYZH-cOEcs1e4emP55xkDag2ZetAXa_ok9U",
+          page: i,
+        },
+      })
+      .then(async (res) => {
+        if (res.data.length != 0) {
+          await res.data.map((item, key) => {
+            products.push(item);
+          });
+          console.log("done : ", i);
+          i++;
+        } else {
+          flag = false;
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .then(() => {});
+  }
 }
 
 async function PushCsv() {
