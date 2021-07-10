c96ba5b36188b597339e36f435bc344e4aede7ce
gentleman1122 <reader2027@gmail.com>
2021-07-10 02:08:53 -0700

    fix csv

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
 const axios = require("axios");
 const fs = require("fs");
+const { Parser } = require("json2csv");
+const json2csvParser = new Parser();
 
 let products = [];
+let flag = true;
+let i = 1;
 async function getProducts(page) {
-  for (let i = 1; i < 250; i++) {
-    await axios
-      .get("http://nodo.mclabels.com/export_prods/0", {
-        params: {
-          token:
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImluZm9Ac2hvY2xlZmNvcnBvcmF0aW9uLmNvbSJ9.Uyn0FgRwtYZH-cOEcs1e4emP55xkDag2ZetAXa_ok9U",
-          page: i,
-        },
-      })
-      .then(async (res) => {
-        if (res.data.length != 0) {
-          await res.data.map((item, key) => {
-            products.push(item);
-          });
-          console.log("done : ", i);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-      .then(() => {});
-  }
+  //  while (flag) {
+  await axios
+    .get("http://nodo.mclabels.com/export_prods/0", {
+      params: {
+        token:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImluZm9Ac2hvY2xlZmNvcnBvcmF0aW9uLmNvbSJ9.Uyn0FgRwtYZH-cOEcs1e4emP55xkDag2ZetAXa_ok9U",
+        page: i,
+      },
+    })
+    .then(async (res) => {
+      if (res.data.length != 0) {
+        await res.data.map((item, key) => {
+          products.push(item);
+        });
+        console.log("done : ", i);
+        i++;
+      } else {
+        flag = false;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    .then(() => {});
+  // }
 }
 
 async function PushCsv() {
   await getProducts();
   const jsonString = JSON.stringify(products);
-  await fs.writeFile("./newProducts.json", jsonString, (err) => {
+  const csv = json2csvParser.parse(products);
+
+  await fs.writeFile("./newProducts.csv", csv, (err) => {
     if (err) {
       console.log("Error writing file", err);
     } else {
